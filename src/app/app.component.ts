2983215ed20ec2e513a387bdb13b6e8ebb2e1e73
Praveen Sripati <pvnsripati@gmail.com>
2021-03-05 23:26:04 +0530

    Implementation of Pagination UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  maxSize = 5;
+  bigTotalItems = 175;
+  bigCurrentPage = 1;
   title = 'spaceX';
   launchFilters = [
     'All Launches',
     'Upcoming Launches',
     'Successful Launches',
-    'Failed Launches'
-  ]
+    'Failed Launches',
+  ];
+  ngOnInit() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -10,6 +11,8 @@ import { SpxHeaderComponent } from './spx-header/spx-header.component';
 import { SpxTableComponent } from './shared/spx-table/spx-table.component';
 import { StatusChipComponent } from './status-chip/status-chip.component';
 import { SpxFiltersModule } from './shared/spx-filters/spx-filters.module';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+
 
 @NgModule({
   declarations: [
@@ -24,6 +27,8 @@ import { SpxFiltersModule } from './shared/spx-filters/spx-filters.module';
     MatSelectModule,
     MatFormFieldModule,
     SpxFiltersModule,
+    FormsModule,
+    PaginationModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent],
