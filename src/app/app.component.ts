9258c03374a79c1a36b89eaf903b37fb5641f2a9
Praveen Sripati <pvnsripati@gmail.com>
2021-03-05 20:59:00 +0530

    Make simple select dynamic w.r.t data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'spaceX';
+  launchFilters = [
+    'All Launches',
+    'Upcoming Launches',
+    'Successful Launches',
+    'Failed Launches'
+  ]
 }
--- src/app/shared/spx-filters/simple-select/simple-select.component.ts
+++ src/app/shared/spx-filters/simple-select/simple-select.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-simple-select',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./simple-select.component.scss']
 })
 export class SimpleSelectComponent implements OnInit {
+  @Input() selectOptions: string[];
 
   constructor() { }
 
