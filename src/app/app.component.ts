6fe50f1e0f09834af4d1d23fc968a9b0c1597ef8
Praveen Sripati <pvnsripati@gmail.com>
2021-03-07 03:35:33 +0530

    Refactor code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,9 @@ import { DataService } from './data.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit, OnDestroy {
+  destroy$: Subject<boolean> = new Subject<boolean>();
+
+  // Table Headers
   tableHeaders = [
     'No:',
     'Launched',
@@ -19,27 +22,34 @@ export class AppComponent implements OnInit, OnDestroy {
     'Launch Status',
     'Rockets'
   ]
-  destroy$: Subject<boolean> = new Subject<boolean>();
+
+  // Pagination Variables
   totalItems = 0;
   maxSize = 5;
   currentPage = 1;
-  selectedOption = 'All Launches';
   launchesPerPage: Launch[];
-  title = 'spaceX';
+
+  // simple-select filter options
+  selectedOption = 'All Launches';
   launchFilters = [
     'All Launches',
     'Upcoming Launches',
     'Successful Launches',
     'Failed Launches',
   ];
+
   constructor(private dataService: DataService) {}
 
   ngOnInit() {
+    if (sessionStorage.getItem("selectedOption")) {
+      console.log(sessionStorage.getItem("selectedOption"));
+      this.selectedOption = sessionStorage.getItem("selectedOption");
+      console.log(this.selectedOption);
+    }
     this.dataService.getLaunches(1).subscribe((data) => {
       if (data) {
         this.launchesPerPage = data['docs'];
         this.totalItems = data['totalDocs'];
-        console.log(data)
       }
     });
   }
@@ -51,14 +61,60 @@ export class AppComponent implements OnInit, OnDestroy {
 
   onSelectedOption(selectedOption) {
     this.selectedOption = selectedOption;
+    sessionStorage.setItem("selectedOption", this.selectedOption);
+    this.getLaunchesData(selectedOption);
   }
 
   pageChanged(e: any) {
-    this.dataService.getLaunches(e.page).subscribe((data) => {
-      if (data) {
-        this.launchesPerPage = data['docs'];
-        console.log(data)
-      }
-    });
+    if (this.selectedOption === 'All Launches') {
+      this.dataService.getLaunches(e.page).subscribe((data) => {
+        this.setLaunchFields(data)
+      });
+    }
+    if (this.selectedOption === 'Upcoming Launches') {
+      this.dataService.getUpcomingLaunches(e.page).subscribe((data) => {
+        this.setLaunchFields(data)
+      });
+    }
+    if (this.selectedOption === 'Successful Launches') {
+      this.dataService.getSuccessfulLaunches(e.page).subscribe((data) => {
+        this.setLaunchFields(data)
+      });
+    }
+    if (this.selectedOption === 'Failed Launches') {
+      this.dataService.getFailedLaunches(e.page).subscribe((data) => {
+        this.setLaunchFields(data)
+      });
+    }
+  }
+
+  getLaunchesData(selectedOption) {
+    if (selectedOption === 'All Launches') {
+      this.dataService.getLaunches(1).subscribe((data) => {
+        this.setLaunchFields(data)
+      });
+    }
+    if (selectedOption === 'Upcoming Launches') {
+      this.dataService.getUpcomingLaunches(1).subscribe((data) => {
+        this.setLaunchFields(data)
+      });
+    }
+    if (selectedOption === 'Successful Launches') {
+      this.dataService.getSuccessfulLaunches(1).subscribe((data) => {
+        this.setLaunchFields(data)
+      });
+    }
+    if (selectedOption === 'Failed Launches') {
+      this.dataService.getFailedLaunches(1).subscribe((data) => {
+      this.setLaunchFields(data);
+      });
+    }
+  }
+
+  setLaunchFields(data) {
+    if (data) {
+      this.launchesPerPage = data['docs'];
+      this.totalItems = data['totalDocs'];
+    }
   }
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,14 +1,46 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
 import { throwError } from 'rxjs';
 import { retry, catchError } from 'rxjs/operators';
-
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
   private API_BASE_URL = 'https://api.spacexdata.com/v4';
+  commonOptions = {
+    select: {
+      name: 1,
+      date_utc: 1,
+      failures: 1,
+      success: 1,
+      upcoming: 1,
+    },
+    populate: [
+      {
+        path: 'payloads',
+        select: {
+          orbit: 1,
+        },
+      },
+      {
+        path: 'rocket',
+        select: {
+          name: 1,
+        },
+      },
+      {
+        path: 'launchpad',
+        select: {
+          name: 1,
+        },
+      },
+    ],
+  };
 
   constructor(private httpClient: HttpClient) {}
 
@@ -26,92 +58,88 @@ export class DataService {
   }
 
   getLaunches(pageNumber: number) {
-    return this.httpClient.post(
-      this.API_BASE_URL  + '/launches/query',
-      {
-        query: {},
-        options: {
-          "page": pageNumber,
-          "select": {
-            "name": 1,
-            "date_utc": 1,
-            "failures": 1,
-            "success": 1,
-            "upcoming": 1
+    return this.httpClient
+      .post(
+        this.API_BASE_URL + '/launches/query',
+        {
+          query: {},
+          options: {
+            page: pageNumber,
+            ...this.commonOptions,
           },
-          "populate": [
-            {
-              "path": "payloads",
-              "select": {
-                "orbit": 1
-              }
-            },
-            {
-              "path": "rocket",
-              "select": {
-                "name": 1
-              }
-            },
-            {
-              "path": "launchpad",
-              "select": {
-                "name": 1
-              }
-            }
-          ]
         },
-      },
-      {
-        headers: new HttpHeaders({
-          'Content-Type': 'application/json',
-        }),
-      }
-    ).pipe(retry(3), catchError(this.handleError));
+        {
+          headers: new HttpHeaders({
+            'Content-Type': 'application/json',
+          }),
+        }
+      )
+      .pipe(retry(3), catchError(this.handleError));
   }
 
-  getUpcomingLaunches() {
-    // return this.httpClient.get(this.API_BASE_URL + '/launches/upcoming');
-    return this.httpClient.post(
-      this.API_BASE_URL  + '/launches/query',
-      {
-        query: {
-          "upcoming": true
-        },
-        options: {
-          "select": {
-            "name": 1,
-            "date_utc": 1,
-            "failures": 1,
-            "success": 1,
-            "upcoming": 1
+  getUpcomingLaunches(pageNumber: number) {
+    return this.httpClient
+      .post(
+        this.API_BASE_URL + '/launches/query',
+        {
+          query: {
+            upcoming: true,
           },
-          "populate": [
-            {
-              "path": "payloads",
-              "select": {
-                "orbit": 1
-              }
-            },
-            {
-              "path": "rocket",
-              "select": {
-                "name": 1
-              }
-            },
-            {
-              "path": "launchpad",
-              "select": {
-                "name": 1
-              }
-            }
-          ]
+          options: {
+            page: pageNumber,
+            ...this.commonOptions,
+          },
         },
-      },
-      {
-        headers: new HttpHeaders({
-          'Content-Type': 'application/json',
-        }),
-      }
-    ).pipe(retry(3), catchError(this.handleError));;
+        {
+          headers: new HttpHeaders({
+            'Content-Type': 'application/json',
+          }),
+        }
+      )
+      .pipe(retry(3), catchError(this.handleError));
+  }
+
+  getSuccessfulLaunches(pageNumber: number) {
+    return this.httpClient
+      .post(
+        this.API_BASE_URL + '/launches/query',
+        {
+          query: {
+            success: true,
+          },
+          options: {
+            page: pageNumber,
+            ...this.commonOptions,
+          },
+        },
+        {
+          headers: new HttpHeaders({
+            'Content-Type': 'application/json',
+          }),
+        }
+      )
+      .pipe(retry(3), catchError(this.handleError));
+  }
+
+  getFailedLaunches(pageNumber: number) {
+    return this.httpClient
+      .post(
+        this.API_BASE_URL + '/launches/query',
+        {
+          query: {
+            "failures.0": { $exists: true },
+          },
+          options: {
+            page: pageNumber,
+            ...this.commonOptions,
+          },
+        },
+        {
+          headers: new HttpHeaders({
+            'Content-Type': 'application/json',
+          }),
+        }
+      )
+      .pipe(retry(3), catchError(this.handleError));
   }
 }
